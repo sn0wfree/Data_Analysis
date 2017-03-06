90ce44d3a66377045ac93d047f3373543aeff0bb
sn0wfree <snowfreedom0815@gmail.com>
2017-03-06 20:20:26 +0000

    add few code

--- AI_based_stock_trader/stock_price_importer.py
+++ AI_based_stock_trader/stock_price_importer.py
@@ -2,7 +2,7 @@
 #-------------------------
 __title__ = "AI trader: Stock Price importer"
 __author__ = "sn0wfree"
-__version__ = 0.0.1
+__version__ = '0.0.1'
 
 """
 This function is a test for a simply ai trader
@@ -10,6 +10,10 @@ This function is a test for a simply ai trader
 #-------------------------
 import pandas as pd
 import os
+import tushare  # for china market
+import yahoo_finance  # for global market
 
 
 if __name__ == '__main__':
+    yahoo = yahoo_finance.Share('IBM')
+    print yahoo.get_open()
