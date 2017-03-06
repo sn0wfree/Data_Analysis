e119e7f65a166cabbed67213492a915a4fd4abfa
sn0wfree <snowfreedom0815@gmail.com>
2017-03-06 19:35:51 +0000

    a ai trader experiment

---
+++ AI_based_stock_trader/stock_price_importer.py
@@ -0,0 +1,15 @@
+# coding=UTF8
+#-------------------------
+__title__ = "AI trader: Stock Price importer"
+__author__ = "sn0wfree"
+__version__ = 0.0.1
+
+"""
+This function is a test for a simply ai trader
+"""
+#-------------------------
+import pandas as pd
+import os
+
+
+if __name__ == '__main__':
