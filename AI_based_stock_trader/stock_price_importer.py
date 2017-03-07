6e0c5f2eeddda5cf60770b17bc6637beccf2ad8f
sn0wfree <snowfreedom0815@gmail.com>
2017-03-07 15:27:10 +0000

    add new function

--- AI_based_stock_trader/stock_price_importer.py
+++ AI_based_stock_trader/stock_price_importer.py
@@ -26,17 +26,43 @@ class SPDCollectionFunction():
             locals()['%s_df' % stock] = pd.DataFrame(yahoo_finance.Share(
                 '%s' % stock).get_historical(Startdate, Enddate))
             locals()['%s_df' % stock] = locals()['%s_df' % stock].iloc[::-1]
+            self.stock_price_dict['%s' % stock] = locals()['%s_df' % stock]
             if output_tocsv == True:
                 locals()['%s_df' % stock].to_csv(
                     "%s_stock_data.csv" % stock, index=False)
-            self.stock_price_dict['%s' % stock] = locals()['%s_df' % stock]
+                print '%s download completed' % stock
+            else:
+                pass
+            # else
+
+    def ShowDailyData(self, Startdate="1989-01-01", Enddate="2017-03-01"):
+        pass
 
 
-def ReadStockList():
+def ReadStockList(path_stock_list):
+    with open(path_stock_list, 'r') as f:
+        stocks = f.readlines()
+        temp = []
+        stock_list = (stock.split(',') for stock in stocks)
+        for s in stock_list:
+
+            if isinstance(s, list):
+                for rank in xrange(len(s)):
+                    if '\n' in s[rank]:
+                        s[rank] = s[rank].split('\n')[0]
+
+                temp.extend(s)
+            elif isinstance(s, str):
+                if '\n' in s:
+                    s = s.split('\n')[0]
+                temp.append(s)
+
+    return temp
 
 
 if __name__ == '__main__':
-    stock_list = ['IBM', 'AAPL', 'GOOG', 'MSFT']
+    #stock_list = ['IBM', 'AAPL', 'GOOG', 'MSFT']
+    stock_list = ReadStockList('stocklist.txt')
     SPDCollectionFunction(stock_list).DownloadDailyData()
 
     # print IBM.get_open()
