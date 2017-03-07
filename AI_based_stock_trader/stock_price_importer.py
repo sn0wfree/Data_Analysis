82be533fe11a3524c174cb60a3b8ad4082b2c6e6
sn0wfree <snowfreedom0815@gmail.com>
2017-03-07 09:53:59 +0000

    create a doc for strategy

--- AI_based_stock_trader/stock_price_importer.py
+++ AI_based_stock_trader/stock_price_importer.py
@@ -14,6 +14,21 @@ import tushare  # for china market
 import yahoo_finance  # for global market
 
 
+class stock_price_data_collection_function():
+
+    def __init__():
+
+    def download_daily_data(stock_list):
+        for stock in stock_list:
+            locals()['%s' % stock] = yahoo_finance.Share('%s' % stock)
+            locals()['%s_df' % stock] = pd.DataFrame(locals()['%s' %
+                                                              stock].get_historical("1989-01-01", "2017-03-01"))
+            locals()['%s_df' % stock] = locals()['%s_df' % stock].iloc[::-1]
+            locals()['%s_df' % stock].to_csv(
+                "%s_stock_data.csv" % stock, index=False)
 if __name__ == '__main__':
-    yahoo = yahoo_finance.Share('IBM')
-    print yahoo.get_open()
+    stock_list = ['IBM', 'AAPL', 'GOOG', 'MSFT']
+
+    # print IBM.get_open()
+
+    #IBM_df.to_csv("ibm_stock_data.csv", index=False)
